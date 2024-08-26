f02e8961c1905b1daf028bcecb6d97d4b20324db
prasathnarayanan6 <ic39553@imail.iitm.ac.in>
2024-08-26 11:50:12 +0530

    cnage again

--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -106,7 +106,7 @@ function Events() {
                                 <select id="countries" className="peer bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 appearance-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         <option value="US">Webinar</option>
                                         <option value="CA">Conference</option>
-                                        <option value="seminar">Seminar</option>
+                                        <option value="se">Seminar</option>
                                         <option value="DE">Workshop</option>
                                         <option value="DE">Masterclass</option>
                                 </select>
